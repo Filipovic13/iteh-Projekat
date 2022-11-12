5dbd19f90dca8a91dfa5c2bd07979f1b95de1328
Filipovic13 <fsaki13@gmail.com>
2022-11-12 14:17:06 +0100

    manje izmene u formatiranju i izmstanju linija koda

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,41 +1,81 @@
-import './App.css';
-import LoginPage from './components/LoginPage';
-import RegisterPage from './components/RegisterPage';
-import NavBar from './components/NavBar';
-import TournamentsPage from './components/TournamentsPage';
-import {BrowserRouter, Routes, Route} from "react-router-dom";
-import {useState} from "react";
-import EventRegistration from './components/EventRegistration';
-
+import "./App.css";
+import LoginPage from "./components/LoginPage";
+import RegisterPage from "./components/RegisterPage";
+import NavBar from "./components/NavBar";
+import TournamentsPage from "./components/TournamentsPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import EventRegistration from "./components/EventRegistration";
+import Products from "./components/Products";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
   const [token, setToken] = useState();
   const [loggedInUser, setLoggedUser] = useState({
-    id:"",
-    name:"",
-    email:"",
+    id: "",
+    name: "",
+    email: "",
   });
 
-  function addLoggedData(logged_user){
+  function addLoggedData(logged_user) {
     setLoggedUser(logged_user);
   }
 
-  function addToken(auth_token){
+  function addToken(auth_token) {
     setToken(auth_token);
   }
+  /////////////////////////////////////////////
+  const [cartNum, increaseCartNum] = useState(0);
+  const [cartItems, addToCart] = useState([]);
 
+  const [products, setProducts] = useState();
+  useEffect(() => {
+    if (products == null) {
+      axios.get("api/products").then((res) => {
+        console.log(res.data);
+        setProducts(res.data);
+      });
+    }
+  }, [products]);
+  //////////////////////////////////////////////
+  function refreshCart() {
+    let newItems = products.filter((prod) => prod.amount > 0);
+    addToCart(newItems);
+  }
+  function addItem(name, id) {
+    console.log("Dodat proizvod: " + name);
+    increaseCartNum(cartNum + 1);
+    // console.log(cartNum);
+    products.forEach((prod) => {
+      if (prod.id === id) {
+        prod.amount++;
+      }
+    });
+    refreshCart();
+  }
 
   return (
     <BrowserRouter className="App">
       <Routes>
-        <Route path='/login' element={<LoginPage addToken={addToken} addLoggedData={addLoggedData} />} />
-        <Route path='/register' element={<RegisterPage/>} />
-        <Route path='/' element={<NavBar token={token} />} >
-            <Route path="tournaments" element={<TournamentsPage/>} />
-            <Route path="eventRegistration" element={<EventRegistration  loggedInUser={loggedInUser}/>} />
+        <Route
+          path="/login"
+          element={
+            <LoginPage addToken={addToken} addLoggedData={addLoggedData} />
+          }
+        />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/" element={<NavBar token={token} />}>
+          <Route path="tournaments" element={<TournamentsPage />} />
+          <Route
+            path="eventRegistration"
+            element={<EventRegistration loggedInUser={loggedInUser} />}
+          />
+          <Route
+            path="products"
+            element={<Products products={products} addItem={addItem} />}
+          />
         </Route>
       </Routes>
-     
     </BrowserRouter>
   );
 }
--- react-frontend/src/components/TournamentsPage.jsx
+++ react-frontend/src/components/TournamentsPage.jsx
@@ -1,25 +1,31 @@
-import React from 'react'
-import OneTournament from './OneTournament';
-import axios from 'axios';
-import {useState, useEffect} from "react";
+import React from "react";
+import OneTournament from "./OneTournament";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function TournamentsPage() {
-    const [tournaments, setTournaments] = useState();
-    useEffect( ()=>{
-        if(tournaments == null){
-            axios.get("api/tournaments").then((res)=>{
-                console.log(res.data);
-                setTournaments(res.data.tournaments);
-            });
-        }
-    },[tournaments] );
-   
+  const [tournaments, setTournaments] = useState();
+  useEffect(() => {
+    if (tournaments == null) {
+      axios.get("api/tournaments").then((res) => {
+        console.log(res.data);
+        setTournaments(res.data.tournaments);
+      });
+    }
+  }, [tournaments]);
+
   return (
     <div>
-        <h3>These are all tournaments</h3>
-        { tournaments == null ? <></> : tournaments.map( (tournament) => <OneTournament tournament={tournament} key={tournament.id} /> ) }
+      <h3>These are all tournaments</h3>
+      {tournaments == null ? (
+        <></>
+      ) : (
+        tournaments.map((tournament) => (
+          <OneTournament tournament={tournament} key={tournament.id} />
+        ))
+      )}
     </div>
-  )
+  );
 }
 
-export default TournamentsPage
+export default TournamentsPage;
