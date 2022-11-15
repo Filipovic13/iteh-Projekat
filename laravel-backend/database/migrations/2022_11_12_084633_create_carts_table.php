87c470cb1cc909eecffca52fb21c7659bb253531
Filipovic13 <fsaki13@gmail.com>
2022-11-15 21:56:50 +0100

    migracije: dodate cene kod proizvoda i napravljena tabela migracije

--- laravel-backend/database/migrations/2022_11_12_084633_create_carts_table.php
+++ laravel-backend/database/migrations/2022_11_12_084633_create_carts_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('image_url');
+            $table->string('price');
             $table->string('quantity');
             $table->foreignId('user_id');
             $table->timestamps();
