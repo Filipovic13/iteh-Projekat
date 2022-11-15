90b1c60d29452426f8884ac9883b4d88a2a49dd7
Filipovic13 <fsaki13@gmail.com>
2022-11-15 21:53:11 +0100

    izmenjenje rute, napravljene posebne za admin korisnika

---
+++ laravel-backend/routes/admin.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\API\AdminAuthController;
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\RegistrationController;
+use App\Http\Controllers\TournamentController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\UserRegistrationController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('admin')->group( function(){
+    Route::post('register', [AdminAuthController::class, 'register']);
+    Route::post('login', [AdminAuthController::class, 'login']);
+
+    Route::middleware('auth:admin,api-admin')->group(function(){
+        Route::resource('tournaments', TournamentController::class)->only(['index','store','update','destroy']);
+        Route::post('logout', [AdminAuthController::class, 'logout']);
+      
+    });
+});
+
+
+?>
