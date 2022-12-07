c0e20fa0709909cbbff1dfaf6d87f0f19461e560
Filipovic13 <fsaki13@gmail.com>
2022-12-07 14:36:19 +0100

    migracija user - dodata rola admin

--- laravel-backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ laravel-backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,14 +15,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('surname');
+            $table->string('name')->nullable();
+            $table->string('surname')->nullable();
             $table->string('email')->unique();
-            $table->string('country');
-            $table->string('city');
-            $table->string('club');
+            $table->string('country')->nullable();
+            $table->string('city')->nullable();
+            $table->string('club')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('role')->default('user');
             $table->rememberToken();
             $table->timestamps();
         });
