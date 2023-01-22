9c96e90357c819fc80d4200bf9c0fea8015e8b9e
Filipovic13 <fsaki13@gmail.com>
2023-01-22 18:10:31 +0100

    za admina dodata stranica za pregled svih registracija

---
+++ react-frontend/src/components/admin/Regitrations/ViewRegistrationsPage.jsx
@@ -0,0 +1,78 @@
+import axios from "axios";
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+
+function ViewRegistrationsPage() {
+   const [registrations, setRegistrations] = useState([]);
+   const [loadind, setLoading] = useState(true);
+   useEffect(() => {
+      var data = {
+         method: "get",
+         url: "api/admin/registrations",
+         headers: {
+            Authorization:
+               "Bearer " + window.sessionStorage.getItem("auth_token"),
+         },
+      };
+
+      axios(data)
+         .then(function (response) {
+            console.log(response.data.registrations);
+            setRegistrations(response.data.registrations);
+            setLoading(false);
+         })
+         .catch(function (error) {
+            console.log(error);
+            setLoading(false);
+         });
+   }, []);
+
+   return (
+      <div className="container-fluid px-4 ">
+         <h1>Registrations</h1>
+
+         <div
+            style={{
+               backgroundColor: "white",
+               borderRadius: "33px",
+               padding: "20px",
+            }}
+         >
+            <table className="table table-striped">
+               <thead>
+                  <tr>
+                     <th scope="col">NAME</th>
+                     <th scope="col">SURNAME</th>
+                     <th scope="col">CATEGORY</th>
+                     <th scope="col">BELT</th>
+                     <th scope="col">EVENT NAME</th>
+                     <th></th>
+                  </tr>
+               </thead>
+               <tbody>
+                  {registrations.map((r) => (
+                     <tr key={r.id}>
+                        <td>{r.name}</td>
+                        <td>{r.surname}</td>
+                        <td>{r.category}</td>
+                        <td>{r.belt}</td>
+                        <td>{r.event_name}</td>
+                        <td>
+                           <button
+                              type="button"
+                              className="btn btn-danger btn-sm"
+                           >
+                              Delete
+                           </button>
+                        </td>
+                     </tr>
+                  ))}
+               </tbody>
+            </table>
+         </div>
+      </div>
+   );
+}
+
+export default ViewRegistrationsPage;
